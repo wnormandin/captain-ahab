ca9101e075348f4f622218490f2cf0aad9aba7d1
wnormandin <normandindev@gmail.com>
2021-10-22 17:45:18 -0500

    Initial commit

---
+++ captain_ahab/utils/constants.py
@@ -0,0 +1,12 @@
+import enum
+import configparser
+
+
+class InputCode(enum.Enum):
+    tab = 0x09
+    a = 0x41
+    b = 0x42
+    d = 0x44
+    e = 0x45
+    r = 0x52
+    f3 = 0x72
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+install_requires = (
+    'pywin32',
+    'numpy',
+    'opencv-python',
+    'pillow',
+    'click'
+)
+
+setup(
+    name='nw-captain-ahab',
+    author_email='normandindev@gmail.com',
+    author='Bill Normandin',
+    packages=find_packages(),
+    license='MIT',
+    description='Captain Ahab handles your fishing needs',
+    install_requires=install_requires
+)
